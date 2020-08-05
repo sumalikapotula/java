5f4547522eb8e5f8292e4159e84aec5265874a46
sumalikapotula <68680647+sumalikapotula@users.noreply.github.com>
2020-08-05 17:42:48 +0530

    done with day19 javaconstructs assignment

--- Employeewagecomputation.java
+++ Employeewagecomputation.java
@@ -2,14 +2,16 @@ public class Employeewagecomputation
 {
         public static final int IS_FULL_TIME=1;
         public static final int IS_PART_TIME=2;
-        public static final int EMP_RATE_PER_HR=100;
+        public static final int EMP_RATE_PER_HR=20;
         public static final int NUM_OF_WORKING_DAYS=20;
-
+        public static final int MAX_HRS_IN_MONTH=100;
         public static void main(String[] args)
         {
         int empHrs=0;
-        int empWage=0, totalempWage=0;
-        for(int day=1;day<=NUM_OF_WORKING_DAYS;day++){
+        int empWage=0, totalempWage=0,totalemphrs=0,totalworkingdays=0;
+        while(totalemphrs<=MAX_HRS_IN_MONTH && totalworkingdays < NUM_OF_WORKING_DAYS)
+        {
+                totalworkingdays++;
                 double empCheck=Math.floor(Math.random()*10)%3;
                 switch((int)empCheck)
                  {
