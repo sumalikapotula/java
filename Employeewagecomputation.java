2553c8c79d8ff0fee27117b1e053da3b3374fcf5
sumalikapotula <68680647+sumalikapotula@users.noreply.github.com>
2020-08-06 18:39:35 +0530

    done with day19 assignment

--- Employeewagecomputation.java
+++ Employeewagecomputation.java
@@ -2,9 +2,9 @@ public class Employeewagecomputation
 {
         public static final int IS_FULL_TIME=1;
         public static final int IS_PART_TIME=2;
-        public static final int EMP_RATE_PER_HR=100;
+        public static final int EMP_RATE_PER_HR=20;
         public static final int NUM_OF_WORKING_DAYS=20;
-
+        public static final int MAX_HRS_IN_MONTH=100;
         public static void main(String[] args)
         {
         int empHrs=0;
